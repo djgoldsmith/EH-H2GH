dac630f3cd31d2a1418994a529e8bd9f1744f0d2
Dan Goldsmith <djgoldsmith@googlemail.com>
2017-11-02 14:26:20 +0000

    Updated Conf to more generic URLS

--- conf.py
+++ conf.py
@@ -21,7 +21,8 @@ BLOG_AUTHOR = "CUEH"  # (translatable)
 BLOG_TITLE = "HitchHikers guide to Ethical Hacking"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "http://djgoldsmith.github.io/EH-H2GH/"
+#SITE_URL = "http://djgoldsmith.github.io/EH-H2GH/"
+SITE_URL = "./"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
